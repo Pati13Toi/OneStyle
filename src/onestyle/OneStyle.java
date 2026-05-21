5386e9b785b9ffd21ed0a43c0d2295931246a3e0
BRENDA <BRENDA@DESKTOP-76CHD84>
2026-05-20 23:35:44 -0500

    Avance del proyecto

---
+++ src/onestyle/OneStyle.java
@@ -0,0 +1,12 @@
+package onestyle;
+
+public class OneStyle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
